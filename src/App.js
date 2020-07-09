fba794a99a453371b521510015424ed0177ad714
kjr <mig@jankjr.dk>
2020-07-09 09:50:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Client,
+  EnvironmentConfiguration
+} from '@neon-exchange/api-client-typescript'
+import {
+  configurePoolSettings
+} from '@neon-exchange/nash-protocol'
 import './App.css';
 
+// This step is really important
+configurePoolSettings(10)
+
+const client = new Client({
+    ...EnvironmentConfiguration.production,
+  host: "localhost:3000",
+  isLocal: true
+})
+
 function App() {
+  const [swapPrices, setSwapPrices] = React.useState(null)
+  const [apiKey, setApiKey] = React.useState("")
+  const [amount, setAmount] = React.useState("0.0044")
+  const [ready, setReady] = React.useState(false)
+  const handleLogin = React.useCallback(() => {
+    client.login(JSON.parse(apiKey)).then(() => {
+      setReady(true)
+    })
+  }, [apiKey])
+
+  React.useEffect(() => {
+    const run = async () => {
+      try {
+        const swapPrices = await fetch('swap-prices')
+        const data = await swapPrices.json()
+        setSwapPrices(data)
+      } catch(e) {
+        console.log(e)
+      }
+    }
+    run()
+    setInterval(run, 15000)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Paste in API key: </p>
+      <textarea cols="200" rows="6" onChange={event => setApiKey(event.target.value)} value={apiKey} /><br/>
+      <button onClick={handleLogin} disabled={apiKey.length === 0}>Login</button>
+      <br />
+        {ready && <>
+          <p>User logged in!</p>
+          <p>
+            Amount to sell: <input type="text" value={amount} onChange={e => setAmount(e.target.value)} /> BTC
+          </p>
+          <button onClick={() => {
+              if (!swapPrices) {
+                return
+              }
+              client.placeLimitOrder(
+                true,
+                {amount: amount, currency: "btc"},
+                "BUY",
+                "GOOD_TIL_CANCELLED",
+                {amount: swapPrices.BTC_USDC.buyPrice.toString(), currencyA: "usdc", currencyB: "btc"},
+                "btc_usdc"
+              )
+          }}>
+            Swap {amount} BTC for {parseFloat(amount) * swapPrices.BTC_USDC.buyPrice} USDC
+          </button>
+        </>
+      }
+      {swapPrices && <p>Current price: {swapPrices.BTC_USDC.buyPrice} USDC / BTC</p>}
     </div>
   );
 }
---
+++ src/setupProxy.js
@@ -0,0 +1,109 @@
+const proxy = require('http-proxy-middleware')
+
+const PROXY_BACKEND = process.env.PROXY_BACKEND || 'http://localhost:4000'
+const PROXY_BTC_EXPLORER_DEV1 = 'https://btc-local-explorer.dev1.nash.io'
+const PROXY_BTC_EXPLORER_DEV2 = 'https://btc-local-explorer.dev2.nash.io'
+const PROXY_BTC_EXPLORER_DEV3 = 'https://btc-local-explorer.dev3.nash.io'
+const PROXY_BTC_EXPLORER_DEV4 = 'https://btc-local-explorer.dev4.nash.io'
+const PROXY_BTC_EXPLORER_QA1 = 'https://btc-local-explorer.qa1.nash.io'
+
+const proxyTimeout = 3600000
+const cookieDomainRewrite = { '*': '' }
+const onProxyRes = (proxyRes, req, res) => {
+  const sc = proxyRes.headers['set-cookie']
+  if (Array.isArray(sc)) {
+    proxyRes.headers['set-cookie'] = sc.map(sc => {
+      return sc
+        .split(';')
+        .filter(v => v.trim().toLowerCase() !== 'secure')
+        .join('; ')
+    })
+  }
+}
+
+module.exports = function(app) {
+  app.use(
+    proxy('/swap-prices', {
+      target: "https://swap-prices.nash.io",
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/api/socket', {
+      target: PROXY_BACKEND,
+      changeOrigin: true,
+      ws: true,
+      proxyTimeout,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/api', {
+      target: PROXY_BACKEND,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+  app.use(
+    proxy('/nash', {
+      target: 'https://nash.io',
+      pathRewrite: {
+        '^/nash': '',
+      },
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/btc-explorer-dev1', {
+      target: PROXY_BTC_EXPLORER_DEV1,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/btc-explorer-dev2', {
+      target: PROXY_BTC_EXPLORER_DEV2,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/btc-explorer-dev3', {
+      target: PROXY_BTC_EXPLORER_DEV3,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/btc-explorer-dev4', {
+      target: PROXY_BTC_EXPLORER_DEV4,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+
+  app.use(
+    proxy('/btc-explorer-qa1', {
+      target: PROXY_BTC_EXPLORER_QA1,
+      changeOrigin: true,
+      cookieDomainRewrite,
+      onProxyRes,
+    })
+  )
+}
